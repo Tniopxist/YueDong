981a6118ad1ca9403a395db0dcb45665577a8740
JING WANG <1203530356@qq.com>
2024-09-05 21:26:59 +0800

    fixed data type

--- app/src/main/java/com/example/app/model/GetAllExerciseRecordResponse.kt
+++ app/src/main/java/com/example/app/model/GetAllExerciseRecordResponse.kt
@@ -2,17 +2,17 @@ package com.example.app.model
 
 data class ExerciseRecord(
     val ID: Int,
-    val avgHeartRate: Int,
-    val bloodOxygenLevel: Int,
+    val avgHeartRate: Float,
+    val bloodOxygenLevel: Float,
     val caloriesBurned: Float,
     val createdAt: String,
     val distance: Float,
     val duration: Int,
     val endTime: String,
     val exerciseType: String,
-    val highBloodPressure: Int,
+    val highBloodPressure: Float,
     val locationPath: String,
-    val lowBloodPressure: Int,
+    val lowBloodPressure: Float,
     val startTime: String,
     val stepsCount: Int,
     val updatedAt: String
